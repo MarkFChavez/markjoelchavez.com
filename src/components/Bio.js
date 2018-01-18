14c6d06cea79ca0140fa0c8a613dc68ca62d3529
Mark Chavez <markchav3z@gmail.com>
2018-01-18 19:15:34 +0800

    Use the work creator instead

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -23,7 +23,7 @@ class Bio extends React.Component {
           }}
         />
         <p>
-          Hi, I'm Mark Chavez. Maintainer of <a href="http://yamda.markjoelchavez.com/">yamda</a>, <a href="https://publicapis.netlify.com">public_apis</a>, <a href="https://jsissues.netlify.com">js_issues</a>, <a href="http://bitcoinpriceindex.netlify.com">bitcoin_index</a> and a whole bunch of open-source projects. Join
+          Hi, I'm Mark Chavez. Creator of <a href="http://yamda.markjoelchavez.com/">yamda</a>, <a href="https://publicapis.netlify.com">public_apis</a>, <a href="https://jsissues.netlify.com">js_issues</a>, <a href="http://bitcoinpriceindex.netlify.com">bitcoin_index</a> and a whole bunch of open-source projects. Join
           me on my adventures as I unfold the good and bad bits about software writing. You can also
           follow me on <a href="https://twitter.com/markisundefined">twitter</a> and <a href="https://github.com/mrkjlchvz">github</a> for more
           goodies. Also, I love <a href="https://twitter.com/search?q=%23oss">#oss</a>!
