201d2ce2e15bcd92a100b591b272a2f342771409
Mark Chavez <markchav3z@gmail.com>
2019-02-05 16:02:17 +0800

    Add text to navigation links

--- src/components/NavigationLinks.js
+++ src/components/NavigationLinks.js
@@ -6,7 +6,11 @@ class NavigationLinks extends React.Component {
   render() {
     return (
       <div>
-        <h1 style={{fontWeight: 'bold', marginTop: rhythm(1/2)}}> Mark Chavez </h1>
+        <h1 style={{fontWeight: 'bold', marginTop: rhythm(1/2), marginBottom: rhythm(1/8)}}> Mark Chavez </h1>
+        <div style={{marginBottom: rhythm(1/4)}}> 
+          <span>Learning how to learn.</span>
+        </div>
+
         <div style={{display: 'flex'}}>
           <div style={{marginRight: rhythm(1/2)}}> 
             <Link 
