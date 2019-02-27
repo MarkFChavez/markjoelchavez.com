e4aabeaebd7f96823eb49bfacac7fca234e0b67c
Mark Chavez <markchav3z@gmail.com>
2019-02-27 09:57:16 +0800

    Use github theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,29 +1,9 @@
 import Typography from 'typography'
-import deYoungTheme from 'typography-theme-de-young'
-import CodePlugin from 'typography-plugin-code'
+import githubTheme from 'typography-theme-github'
 
-const typography = new Typography(deYoungTheme)
+githubTheme.baseFontSize = '20px'
 
-deYoungTheme.plugins = [new CodePlugin()]
-deYoungTheme.baseLineHeight = 1.4
-deYoungTheme.blockMarginBottom = 0.75
-deYoungTheme.overrideThemeStyles = ({ rhythm }) => ({
-  "blockquote > h1, blockquote > h2, blockquote > h3, blockquote > h4": {
-    marginTop: 0,
-  },
-  "li > p": {
-    marginBottom: rhythm(1 / 2),
-  },
-  "p code": {
-    fontSize: "75%",
-  },
-  "tt,code": {
-    fontSize: "85%",
-  },
-  pre: {
-    lineHeight: 1.22,
-  },
-})
+const typography = new Typography(githubTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
