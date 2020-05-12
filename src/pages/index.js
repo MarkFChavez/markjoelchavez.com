7111a6b053025ef95bbe60c698ac91ded1b430f3
Mark Chavez <markchav3z@gmail.com>
2020-05-12 14:46:10 +0800

    Update bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ class RootPage extends React.Component {
           </p>
 
           <p style={{textAlign: 'justify'}}>
-            I want to share my thoughts about programming in general. I'd like this to be my personal journal (hopefully yours too!) for whenever I need to recall something.
+            I want to share my thoughts about programming in general. I'd like this to be my personal journal (hopefully yours too!) for whenever I or we need to recall concepts we forget.
           </p>
         </div>
       
