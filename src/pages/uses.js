63e45697766f782bb60cec0fda0c9185b3a42fb1
Mark Chavez <mjfchavez@gmail.com>
2020-08-22 13:50:02 +0800

    feat: add instagram link to photos

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -20,7 +20,7 @@ class UsesPage extends React.Component {
           <li> I use <a href="https://nexstand.eu/products/nexstand-k2">NEXSTAND</a> adjustable stand for my laptop. I bring this wherever I go! </li>
           <li> I type on a <a href="https://www.amazon.com/Logitech-K380-Multi-Device-Bluetooth-Keyboard/dp/B0148NPH9I">K380 Logitech Keyboard</a> which I find to be surprisingly durable and fun to use. </li>
           <li> I use <a href="https://www.amazon.com/Logitech-Master-Wireless-Mouse-High-precision/dp/B00TZR3WRM">Logitech MX Master</a> for all things (e.g. gaming, coding, etc) </li>
-          <li> <a href="https://www.amazon.com/Ricoh-GR-II-Digital-Camera/dp/B00ZY9JJEU">Ricoh GR II</a> for photography (I mostly do street) </li>
+          <li> <a href="https://www.amazon.com/Ricoh-GR-II-Digital-Camera/dp/B00ZY9JJEU">Ricoh GR II</a> for photography. I mostly do street check out my <a href="https://www.instagram.com/_markandwhite/">photos</a>! </li>
         </ul>
       </div>
     )
