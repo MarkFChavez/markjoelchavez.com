f4a4e8b2cb6a8f90d9e55036c9c362a76d7f5e78
Mark Chavez <mjfchavez@gmail.com>
2020-08-22 13:57:35 +0800

    feat: change text

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ class RootPage extends React.Component {
           </p>
 
           <p style={{textAlign: 'justify'}}>
-            I want to share my thoughts about programming in general. I'd like this to be my personal journal (hopefully yours too!) for whenever I or we need to recall concepts we forget.
+            I want to share my thoughts about programming in general. But this is mostly a personal journal for whenever I need to recall a particular topic or concept because I'm quite forgetful! 😃
           </p>
         </div>
       
